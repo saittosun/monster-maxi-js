85140aeaecb3a08442fc84f4295e6f60007ed889
tostos <saittosun2@gmail.com>
2020-08-22 18:04:26 +0200

    adding more if statements 7 a strong attack functionality before refactoring

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,6 +2,7 @@
 
 // we as a developer set up as a global value which you just hardcode into your code, it's also quite common to use all uppercase characters there and separate words with an underscore, to make it really clear that this is a global value which you just hardcoded into the code as we did it here.
 const ATTACK_VALUE = 10;
+const STRONG_ATTACK_VALUE = 17;
 const MONSTER_ATTACK_VALUE = 14;
 
 let chosenMaxLife = 100;
@@ -19,9 +20,24 @@ function attackHandler() {
     alert("you won");
   } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
     alert("you lost");
-  } else if (currentPlayerHealth <= 0 && currentMonsterHealth <=0 ) {
-    alert('you have a draw.')
+  } else if (currentPlayerHealth <= 0 && currentMonsterHealth <= 0) {
+    alert("you have a draw.");
+  }
+}
+
+function strongAttackHandler() {
+  const damage = dealMonsterDamage(STRONG_ATTACK_VALUE);
+  currentMonsterHealth -= damage;
+  const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
+  currentPlayerHealth -= playerDamage;
+  if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
+    alert("you won");
+  } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
+    alert("you lost");
+  } else if (currentPlayerHealth <= 0 && currentMonsterHealth <= 0) {
+    alert("you have a draw.");
   }
 }
 
 attackBtn.addEventListener("click", attackHandler);
+strongAttackBtn.addEventListener("click", strongAttackHandler);
--- assets/scripts/vendor.js
+++ assets/scripts/vendor.js
@@ -1,12 +1,12 @@
 // jshint esversion: 9
-const monsterHealthBar = document.getElementById('monster-health');
-const playerHealthBar = document.getElementById('player-health');
-const bonusLifeEl = document.getElementById('bonus-life');
+const monsterHealthBar = document.getElementById("monster-health");
+const playerHealthBar = document.getElementById("player-health");
+const bonusLifeEl = document.getElementById("bonus-life");
 
-const attackBtn = document.getElementById('attack-btn');
-const strongAttackBtn = document.getElementById('strong-attack-btn');
-const healBtn = document.getElementById('heal-btn');
-const logBtn = document.getElementById('log-btn');
+const attackBtn = document.getElementById("attack-btn");
+const strongAttackBtn = document.getElementById("strong-attack-btn");
+const healBtn = document.getElementById("heal-btn");
+const logBtn = document.getElementById("log-btn");
 
 function adjustHealthBars(maxLife) {
   monsterHealthBar.max = maxLife;
@@ -18,6 +18,7 @@ function adjustHealthBars(maxLife) {
 function dealMonsterDamage(damage) {
   const dealtDamage = Math.random() * damage;
   monsterHealthBar.value = +monsterHealthBar.value - dealtDamage;
+  // console.log(monsterHealthBar);
   return dealtDamage;
 }
 
