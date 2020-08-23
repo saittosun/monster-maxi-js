0d0a39ca71358977be2dd14f26b98962a2f0c1b5
tostos <saittosun2@gmail.com>
2020-08-23 08:33:41 +0200

    validating user input

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -6,7 +6,13 @@ const STRONG_ATTACK_VALUE = 17;
 const MONSTER_ATTACK_VALUE = 14;
 const HEAL_VALUE = 20;
 
-let chosenMaxLife = 100;
+const enteredNumber = prompt('maximum life for you and the monster.', '100');
+let chosenMaxLife = parseInt(enteredNumber);
+
+if (isNaN(chosenMaxLife) || chosenMaxLife <= 0) {
+  chosenMaxLife = 100;
+}
+
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
 let hasBonusLife = true;
